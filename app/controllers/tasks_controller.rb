4e5e25c70acfadb9db92f95c960b08fe846ffc4f
riho.nagaoka <riho.nagaoka@techacademy.jp>
2020-10-06 05:24:49 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+  
   def index
       @tasks =Task.all
   end
 
   def show
-      @task =Task.find(params[:id])
   end
 
   def new
@@ -15,20 +16,18 @@ class TasksController < ApplicationController
       @task = Task.new(task_params)
 
     if @task.save
-      flash[:success] = 'タスクが投稿されました'
+      flash[:success] = 'タスクが作成されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'タスクが投稿されません'
+      flash.now[:danger] = 'タスクが作成されていません'
       render :new
     end
   end
 
   def edit
-      @task = Task.find(params[:id])
   end
 
   def update
-      @task = Task.find(params[:id])
       if @task.update(task_params)
           flash[:success] = 'Task は正常に編集されました'
           redirect_to @task
@@ -39,7 +38,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-      @task = Task.find(params[:id])
       @task.destroy
       
       flash[:success] ='Task は正常に削除されました'
@@ -47,6 +45,11 @@ class TasksController < ApplicationController
   end
   
  private
+ 
+ def set_message
+   @task=task.find(params[:id])
+ end
+ 
  def task_params
    params.require(:task).permit(:content)
  end
