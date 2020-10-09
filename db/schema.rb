5b57501e2920b3ca1d95d265942a205015d896c4
riho.nagaoka <riho.nagaoka@techacademy.jp>
2020-10-09 07:56:08 +0000

    validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_110744) do
+ActiveRecord::Schema.define(version: 2020_10_09_044526) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_10_02_110744) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
